39ecbb2dd59ba2a37eefb52a019bb8828a19debd
vivek <codecrafter67@gmail.com>
2026-01-22 13:46:59 +0530

    put id in quotes

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ app = FastAPI()
 
 
 products = [
-    {id:1,'name':"Laptop",'price':50000,'quantity':10},
-    {id:2,'name':"Headphone",'price':20000,'quantity':4},
-    {id:3,'name':"IPhone",'price':120000,'quantity':7}
+    {'id':1,'name':"Laptop",'price':50000,'quantity':10},
+    {'id':2,'name':"Headphone",'price':20000,'quantity':4},
+    {'id':3,'name':"IPhone",'price':120000,'quantity':7}
 ]
 
 @app.get("/")
